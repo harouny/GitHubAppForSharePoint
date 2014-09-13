19a28515f23ed7b4bc83253ff875afd66a6f7929
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-09-13 11:10:12 +1000

    initial commit

---
+++ GithubAppForSharePoint.App/Features/Feature1/Feature1.feature
@@ -0,0 +1,8 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="f26c9b90-c7f6-4520-97c8-3101b9f2cbb1" description="SharePoint App Feature" featureId="f26c9b90-c7f6-4520-97c8-3101b9f2cbb1" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="GithubAppForSharePoint.App Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="7e62070d-8216-488f-994f-f0b8a53f1c2c" />
+    <projectItemReference itemId="2a9b6c16-af8d-4927-a678-2375b53fc297" />
+    <projectItemReference itemId="db613fca-c5f6-4222-b46b-36cef5001b44" />
+  </projectItems>
+</feature>
---
+++ GithubAppForSharePoint.App/Scripts/app/app.js
@@ -0,0 +1,40 @@
+﻿(function (require, define) {
+    "use strict";
+
+    require.config(
+    {
+        //all paths are relative to "/Scripts"
+        baseUrl: 'Scripts',
+        paths:
+        {
+            'app': './app',
+            'underscore': './underscore-min',
+            'jquery': './jquery-2.1.1.min',
+            'angular': './angular.min'
+        },
+        shim:
+        {
+            'underscore':
+            {
+                exports: '_'
+            },
+            'angular' :
+            {
+                exports: 'angular'
+            }
+        }
+    });
+
+    define("app", ["angular", "app/github/githubModule"],
+        function (angular, githubModule) {
+            angular.bootstrap(document.getElementsByTagName("body")[0],
+                              [githubModule.name]);
+            return githubModule;
+        }
+    );
+
+    require(["app"], function (githubModule) {
+        // Application has bootstrapped and started...
+    });
+
+}(require, define));
