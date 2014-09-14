41f4e01ae3f57f7c55601cf9a547524a0a5d9708
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-09-14 10:11:22 +1000

    add missing files

---
+++ GithubAppForSharePoint.App/App/common/chromeControlDirective.js
@@ -0,0 +1,24 @@
+﻿(function (define) {
+    "use strict";
+
+    define([
+        "common/commonModule",
+        "common/chromeControlDirective.ctrl"
+    ],
+    function (commonModule) {
+        commonModule.directive("commonChromeControl",
+            [function () {
+                return {
+                    restrict: 'E',
+                    replace: false,
+                    controller: 'chromeControlDirective.ctrl',
+                    scope: {},
+                    templateUrl: "../App/common/chromeControlDirective.tpl.html",
+                    link: function ($scope, element) {
+                        $scope.chromeControlContainerId = "chrome-control-container";
+                    }
+                };
+            }]);
+    });
+
+}(window.define));
