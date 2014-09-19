dbc386b495a82a0205a536ffef445653181a6696
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-09-20 00:10:58 +1000

    change window to angular $window in notification service

--- GithubAppForSharePoint.App/App/common/notificationService.js
+++ GithubAppForSharePoint.App/App/common/notificationService.js
@@ -1,9 +1,10 @@
-﻿(function (define) {
+﻿// ReSharper disable InconsistentNaming
+(function (define) {
     "use strict";
 
     define(["common/common", "spCore"],
         function (common) {
-            common.factory("notificationService", ["$log", function ($log) {
+            common.factory("notificationService", ["$log", "$window", function ($log, $window) {
 
                 function error(title, messege) {
                     notify(title, messege, "../Images/error.png");
@@ -15,15 +16,15 @@
 
                 function notify(title, messege, imageUrl) {
 
-                    var notificationData = new window.SPStatusNotificationData(
+                    var notificationData = new $window.SPStatusNotificationData(
                         "", //text
-                        window.STSHtmlEncode(messege), //subText
+                        $window.STSHtmlEncode(messege), //subText
                         imageUrl, //imageUrl
                         null); //sip
 
-                    var notification = new window.SPNotification(
-                        window.SPNotifications.ContainerID.Status, //containerId
-                        window.STSHtmlEncode(title), //message
+                    var notification = new $window.SPNotification(
+                        $window.SPNotifications.ContainerID.Status, //containerId
+                        $window.STSHtmlEncode(title), //message
                         false, //isSticky
                         null,  //tooltip
                         null,  //onClickCallback
@@ -41,3 +42,4 @@
         });
 
 }(window.define));
+// ReSharper restore InconsistentNaming
