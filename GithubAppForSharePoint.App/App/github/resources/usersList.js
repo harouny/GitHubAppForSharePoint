8c8ec480d224ef37a6d5e27d5cc3c5ac5430f9fa
Ahmed EL-Harouny <ahmedelharouny@yahoo.com>
2014-09-22 20:57:55 +1000

    add sp list definitions

---
+++ GithubAppForSharePoint.App/App/github/resources/usersList.js
@@ -0,0 +1,18 @@
+﻿(function (define) {
+    "use strict";
+
+    define(["github/github"],
+        function (common) {
+            common.factory("usersList",
+                ["$resource", function ($resource) {
+                    return $resource('../_api/web/lists/getbytitle(\'Users\')', {}, {
+                        get: {
+                            params: {
+                                '$select': 'GitHubUserName,UserName'
+                            }
+                        }
+                    });
+                }]);
+        });
+
+}(window.define));
